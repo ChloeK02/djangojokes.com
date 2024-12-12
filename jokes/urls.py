d40190ccd31b4ba511cec3240bdfffc192c9608a
ChloeK02 <ckiedaisch09@gmail.com>
2024-12-11 17:00:46 -0800

    creating and updating jokes - joke form

--- jokes/urls.py
+++ jokes/urls.py
@@ -4,9 +4,15 @@ from .views import JokeListView
 
 from .views import JokeDetailView, JokeListView
 
+from .views import (
+    JokeCreateView, JokeDetailView, JokeListView, JokeUpdateView
+)
+
 app_name = 'jokes'
 urlpatterns = [
     path('', JokeListView.as_view(), name='list'),
     path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+    path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
+    path('joke/create/', JokeCreateView.as_view(), name='create'),
 ]
 
--- jokes/views.py
+++ jokes/views.py
@@ -6,8 +6,18 @@ from .models import Joke
 
 from django.views.generic import DetailView, ListView
 
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+
 class JokeListView(ListView):
     model = Joke
 
 class JokeDetailView(DetailView):
     model = Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
