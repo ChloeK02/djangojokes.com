e39fc585fc3b315a51de52ef642a3e8776b07015
ChloeK02 <ckiedaisch09@gmail.com>
2024-12-11 16:53:32 -0800

    create a detail view

--- jokes/views.py
+++ jokes/views.py
@@ -4,5 +4,10 @@ from django.views.generic import ListView
 
 from .models import Joke
 
+from django.views.generic import DetailView, ListView
+
 class JokeListView(ListView):
+    model = Joke
+
+class JokeDetailView(DetailView):
     model = Joke
